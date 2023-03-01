857a4db49ac2532e7c8dd2ea2e8da9440f69ad4b
alefernandezoficial <alefernandezoficial@gmail.com>
2023-03-01 10:33:25 -0300

    Updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Team from "./components/pages/Team";
+import Contact from "./components/pages/Contact";
+import Cart from "./components/pages/Cart";
+
+import NavBar from "./components/NavBar";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <NavBar/>
+
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="about" element={ <About /> } />
+        <Route path="team" element={ <Team /> } />
+        <Route path="contact" element={ <Contact /> } />
+        <Route path="cart" element={ <Cart /> } />
+      </Routes>
+
+    </div >
   );
 }
 
